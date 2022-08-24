c333bd9cc471777f4723e2075c92043a2b624a2d
jbohn3353 <jbohn3353@gmail.com>
2022-08-24 14:51:28 -0600

    Finder hopefully complete

---
+++ finder-app/finder.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Counts string occurences in a directory
+# Usage:
+# ./finder.sh [dir] [string]
+# Author: James Bohn
+
+READDIR=$1
+READSTR=$2
+
+if [ $# -lt 2 ]; then
+    echo "Not enough arguments."
+    exit 1
+fi
+
+if ! [ -d ${READDIR} ]; then
+    echo "Invalid search directory."
+    exit 1
+fi
+
+FILES=$(grep -c ${READSTR} ${READDIR}/* | wc -l)
+LINES=$(grep -0 ${READSTR} ${READDIR}/* | wc -l)
+
+echo "The number of files are ${FILES} and the number of matching lines are ${LINES}"
