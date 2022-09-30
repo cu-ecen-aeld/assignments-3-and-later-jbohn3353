08a9e3db4241e7ba63c192b84fc446f70dde2e99
jbohn3353 <jbohn3353@gmail.com>
2022-09-30 12:49:12 -0600

    Add start-stop script

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+# Simple script to start/stop our socket server as a daemon, adapted from code
+# provided in class and mastering embedded linux programming chapter 10
+# Usage:
+#   ./aesdsocket-start-stop {start|stop}
+
+case "$1" in
+    start)
+        echo "Starting aesdsocket in daemon mode"
+        start-stop-daemon --start --name aesdsocket -startas /usr/bin/aesdsocket
+        ;;
+    stop)
+        echo "Stopping aesdsocket"
+        start-stop-daemon --stop --name aesdsocket
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+    exit 1
+esac
+
+exit 0
