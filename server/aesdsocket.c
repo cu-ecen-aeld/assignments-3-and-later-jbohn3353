d050177e56eb17b963553909810675fdfc41c15d
jbohn3353 <jbohn3353@gmail.com>
2022-10-09 09:50:55 -0600

    implement assignment 6 part 1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -20,13 +20,29 @@
 #include <fcntl.h>
 #include <syslog.h>
 #include <stdbool.h>
+#include <pthread.h>
+#include <time.h>
+#include "queue.h"
 #include "vector.h"
 
 #define PORT "9000"  // the port users will be connecting to
 #define DATA_FILE "/var/tmp/aesdsocketdata"
+#define TIMESTAMP_SIZE 100
 #define CHUNK_SIZE 200
 
 #define BACKLOG 10	 // how many pending connections queue will hold
+
+// Struct to manage the data file across threads
+struct shared_file {
+	int fd;
+	pthread_mutex_t mtx;
+};
+
+struct thread_data {
+	pthread_t thread;
+	int client_fd;
+	bool complete;
+};
 
 // Struct holding fd's to close and pointers to memory/structs to free
 struct cleanup_data {
@@ -34,12 +50,24 @@ struct cleanup_data {
 	int sock_fd;
 	int new_fd;
 	int data_fd;
+};
 
+// Struct holding fd's to close and pointers to memory/structs to free
+struct thread_cleanup_data {
+	struct thread_data *t_data;
 	vector *recv_vec;
 	vector *send_vec;
 };
 
+// SLIST.
+typedef struct slist_data_s slist_data_t;
+struct slist_data_s {
+    struct thread_data td;
+    SLIST_ENTRY(slist_data_s) entries;
+};
+
 static bool sig_received = false;
+static struct shared_file data_file;
 
 void sig_handler(int s) {
 	sig_received = true;
@@ -60,44 +88,213 @@ void cleanup(struct cleanup_data *cd){
 	freeaddrinfo(cd->servinfo);
 	close(cd->sock_fd);
 	close(cd->new_fd);
-	close(cd->data_fd);
+
+	close(data_file.fd);
+	pthread_mutex_destroy(&data_file.mtx);
+	closelog();
+}
+
+void thread_cleanup(struct thread_cleanup_data *cd){
 	vector_close(cd->recv_vec);
 	vector_close(cd->send_vec);
-	closelog();
+	
+	cd->t_data->complete = true;
+}
+
+void write_timestamp(union sigval sigval){
+	char time_string[TIMESTAMP_SIZE] = "timestamp:";
+	time_t now = time(0);
+	struct tm now_tm = *localtime(&now);
+	strftime(time_string + sizeof("timestamp:")-1, sizeof(time_string), "%a, %d %b %Y %T %z%n", &now_tm);
+
+
+	pthread_mutex_lock(&data_file.mtx);
+	if(write(data_file.fd, time_string, strlen(time_string)) == -1){
+		pthread_mutex_unlock(&data_file.mtx);
+		syslog(LOG_ERR, "error writing data to file");
+		return;				
+	}
+	pthread_mutex_unlock(&data_file.mtx);
+}
+
+void *handle_connection(void *thread_param){
+	int received, written, rv;
+	vector recv_vec, send_vec;
+	char *new_line;
+	char read_char;
+	char recv_buf[CHUNK_SIZE];
+	struct thread_cleanup_data cd;
+
+	struct thread_data *t_data = (struct thread_data *) thread_param;
+
+	// Set up data for easy cleanup
+	recv_vec.buf = NULL;
+	send_vec.buf = NULL;
+	cd.t_data = t_data;
+	cd.recv_vec = &recv_vec;
+	cd.send_vec = &send_vec;
+
+	// Clear receive buffer to prepare for receive
+	if(vector_init(&recv_vec)){
+		syslog(LOG_ERR, "vec_init fail\n");
+		thread_cleanup(&cd);
+		return NULL;
+	}
+	
+	// Loop until client closes connection
+	while(!sig_received){
+		received = 0;
+
+		// Call receive until we've received a newline
+		while(!(new_line = vector_find(&recv_vec, recv_vec.len - received, '\n'))){
+
+			// Perform non-blocking receive
+			do {
+				received = recv(t_data->client_fd, recv_buf, CHUNK_SIZE, 0);
+			} while (received == -1 && errno == EAGAIN);
+
+			if(received == -1) {
+				syslog(LOG_ERR, "error on syscall: recv");
+				thread_cleanup(&cd);
+				return NULL;
+			}
+			else if(received > 0){
+				if(vector_append(&recv_vec, recv_buf, received)){
+					syslog(LOG_ERR, "vec_append fail\n");
+					thread_cleanup(&cd);
+					return NULL;
+				}
+			}
+			else {
+				// Connection was terminated, follow into the if below
+				// since double break isn't well defined
+				break;
+			}
+		}
+
+		if(received == 0){
+			break;
+		}
+
+		// Write from the receive buffer into the data file one line at a time
+		written = 0;
+		while((new_line = vector_find(&recv_vec, written, '\n'))){
+			pthread_mutex_lock(&data_file.mtx);
+			if((rv = write(data_file.fd, recv_vec.buf+written, new_line + 1 - (char *)(recv_vec.buf+written))) == -1){
+				pthread_mutex_unlock(&data_file.mtx);
+				syslog(LOG_ERR, "error writing data to file");
+				thread_cleanup(&cd);
+				return NULL;				
+			}
+			pthread_mutex_unlock(&data_file.mtx);
+			written += rv;
+		}
+
+		// If we have have a extra data in the receive buffer, carry it thourgh
+		// to the next packet, otherwise reset the buffer
+		if(written < recv_vec.len){
+			vector_carryover(&recv_vec, written);
+		}
+		else{
+			vector_close(&recv_vec);
+			vector_init(&recv_vec);
+		}
+
+		if(vector_init(&send_vec)){
+			syslog(LOG_ERR, "vec_init fail\n");
+			thread_cleanup(&cd);
+			return NULL;
+		}
+
+		pthread_mutex_lock(&data_file.mtx);
+		// Seek back to start of file for read
+		if(lseek(data_file.fd, 0, SEEK_SET) == -1){
+			pthread_mutex_unlock(&data_file.mtx);
+			syslog(LOG_ERR, "error seeking to start of file");
+			thread_cleanup(&cd);
+			return NULL;
+		}
+		pthread_mutex_unlock(&data_file.mtx);
+
+		// Loop through chars in file
+		while(1){
+			
+			pthread_mutex_lock(&data_file.mtx);
+			if((rv = read(data_file.fd, &read_char, 1)) != 1){
+				pthread_mutex_unlock(&data_file.mtx);
+				break;
+			}
+			pthread_mutex_unlock(&data_file.mtx);
+
+			// Apeend each char to the line buffer
+			if(vector_append(&send_vec, &read_char, 1)){
+				syslog(LOG_ERR, "vec_append fail\n");
+				thread_cleanup(&cd);
+				return NULL;
+			}
+
+			// If new line is found, send off and clear/re-init line buffer
+			if( read_char == '\n'){
+				// Perform non-blocking send
+				do {
+					rv = send(t_data->client_fd, send_vec.buf, send_vec.len, 0);
+				} while (rv == -1 && errno == EAGAIN);
+				if(rv == -1) {
+					syslog(LOG_ERR, "error on syscall: send");
+					thread_cleanup(&cd);
+					return NULL;
+				}
+
+				vector_close(&send_vec);
+				if(vector_init(&send_vec)){
+					syslog(LOG_ERR, "vec_init fail\n");
+					thread_cleanup(&cd);
+					return NULL;
+				}
+				continue;
+			}
+		}
+
+		// clear buffer to prepare for next receive
+		vector_close(&send_vec);
+	}
+
+	thread_cleanup(&cd);
+	return NULL;
 }
 
 int main(int argc, char **argv) {
 	int sock_fd=0; // listen on sock_fd
 	int new_fd=0; // new connections on new_fd
-	int data_fd=0; // data file
 	struct addrinfo hints, *servinfo=NULL, *p;
 	struct sockaddr_storage their_addr; // connector's address information
 	struct sigaction sa;
+	struct sigevent sev;
 	socklen_t sin_size;
+	timer_t timer;
 	int yes=1;
 	char s[INET6_ADDRSTRLEN];
-	char recv_buf[CHUNK_SIZE];
-	char *new_line;
 	int rv;
-	int received;
-	int written;
-	vector recv_vec, send_vec;
-	char read_char;
 	int flags;
 	pid_t pid;
 
 	// setup stuff to cleanup
 	struct cleanup_data cd;
-	recv_vec.buf = NULL;
-	send_vec.buf = NULL;
 	cd.servinfo = servinfo;
 	cd.sock_fd = sock_fd;
 	cd.new_fd = new_fd;
-	cd.data_fd = data_fd;
-	cd.recv_vec = &recv_vec;
-	cd.send_vec= &send_vec;
+
+	// setup shared data file
+	data_file.fd = 0;
+	pthread_mutex_init(&data_file.mtx, NULL);
 
 	openlog("server_log", LOG_CONS | LOG_NDELAY, LOG_USER);
+
+	// setup SLIST
+	slist_data_t *datap=NULL;
+	slist_data_t *nextp=NULL;
+    SLIST_HEAD(slisthead, slist_data_s) head;
+    SLIST_INIT(&head);
 
 	// install handler for SIGINT and SIGTERM
 	sa.sa_handler = sig_handler;
@@ -214,12 +411,26 @@ int main(int argc, char **argv) {
 	}
 
 	// Create/wipe data file
-	data_fd = open(DATA_FILE, O_CREAT | O_RDWR | O_TRUNC,0644);
-	if(data_fd == -1) {
+	data_file.fd = open(DATA_FILE, O_CREAT | O_RDWR | O_TRUNC,0644);
+	if(data_file.fd == -1) {
 		syslog(LOG_ERR, "error creating data file");
 		cleanup(&cd);
 		return -1;
 	}
+
+	memset(&sev, 0, sizeof(struct sigevent));
+	sev.sigev_notify = SIGEV_THREAD;
+	sev.sigev_value.sival_ptr = &data_file;
+	sev.sigev_notify_function = write_timestamp;
+	timer_create(CLOCK_REALTIME, &sev, &timer);
+
+	struct timespec sleep_time;
+	sleep_time.tv_sec = 10;
+	sleep_time.tv_nsec = 0;
+	struct itimerspec spec;
+	spec.it_value = sleep_time;
+	spec.it_interval = sleep_time;
+	timer_settime(timer, 0, &spec, NULL);
 
 	syslog(LOG_DEBUG, "waiting for connections...\n");
 
@@ -247,122 +458,23 @@ int main(int argc, char **argv) {
 			s, sizeof(s));
 		syslog(LOG_DEBUG, "Accepted connection from %s\n", s);
 
-		// Clear receive buffer to prepare for receive
-		if(vector_init(&recv_vec)){
-			syslog(LOG_ERR, "vec_init fail\n");
-			cleanup(&cd);
-			return -1;
-		}
+		datap = malloc(sizeof(slist_data_t));
+		datap->td.client_fd = new_fd;
+		datap->td.complete = false;
 		
-		// Loop until client closes connection
-		while(!sig_received){
-			received = 0;
+		SLIST_INSERT_HEAD(&head, datap, entries);
 
-			// Call receive until we've received a newline
-			while(!(new_line = vector_find(&recv_vec, recv_vec.len - received, '\n'))){
+		pthread_create(&datap->td.thread, NULL, handle_connection, &datap->td);
 
-				// Perform non-blocking receive
-				do {
-					received = recv(new_fd, recv_buf, CHUNK_SIZE, 0);
-				} while (received == -1 && errno == EAGAIN);
-
-				if(received == -1) {
-					syslog(LOG_ERR, "error on syscall: recv");
-					cleanup(&cd);
-					return -1;
-				}
-				else if(received > 0){
-					if(vector_append(&recv_vec, recv_buf, received)){
-						syslog(LOG_ERR, "vec_append fail\n");
-						cleanup(&cd);
-						return -1;
-					}
-				}
-				else {
-					// Connection was terminated, follow into the if below
-					// since double break isn't well defined
-					break;
-				}
-			}
-
-			// Connection closed by client
-			if(received == 0){
+		SLIST_FOREACH_SAFE(datap, &head, entries, nextp){
+			if(datap->td.complete){
+				pthread_join(datap->td.thread, NULL);
 				syslog(LOG_DEBUG, "Closed connection from %s\n", s);
-				break;
+				close(datap->td.client_fd);
+				SLIST_REMOVE(&head, datap, slist_data_s, entries);
+				free(datap);
 			}
-
-			// Write from the receive buffer into the data file one line at a time
-			written = 0;
-			while((new_line = vector_find(&recv_vec, written, '\n'))){
-				if((rv = write(data_fd, recv_vec.buf+written, new_line + 1 - (char *)(recv_vec.buf+written))) == -1){
-					syslog(LOG_ERR, "error writing data to file");
-					cleanup(&cd);
-					return -1;				
-				}
-				written += rv;
-			}
-
-			// If we have have a extra data in the receive buffer, carry it thourgh
-			// to the next packet, otherwise reset the buffer
-			if(written < recv_vec.len){
-				vector_carryover(&recv_vec, written);
-			}
-			else{
-				vector_close(&recv_vec);
-				vector_init(&recv_vec);
-			}
-
-			if(vector_init(&send_vec)){
-				syslog(LOG_ERR, "vec_init fail\n");
-				cleanup(&cd);
-				return -1;
-			}
-
-			// Seek back to start of file for read
-			if(lseek(data_fd, 0, SEEK_SET) == -1){
-				syslog(LOG_ERR, "error seeking to start of file");
-				cleanup(&cd);
-				return -1;
-			}
-
-			// Loop through chars in file
-			while(read(data_fd, &read_char, 1) == 1){
-				// Apeend each char to the line buffer
-				if(vector_append(&send_vec, &read_char, 1)){
-					syslog(LOG_ERR, "vec_append fail\n");
-					cleanup(&cd);
-					return -1;
-				}
-
-				// If new line is found, send off and clear/re-init line buffer
-				if( read_char == '\n'){
-					// Perform non-blocking send
-					do {
-						rv = send(new_fd, send_vec.buf, send_vec.len, 0);
-					} while (rv == -1 && errno == EAGAIN);
-					if(rv == -1) {
-						syslog(LOG_ERR, "error on syscall: send");
-						cleanup(&cd);
-						return -1;
-					}
-
-					vector_close(&send_vec);
-					if(vector_init(&send_vec)){
-						syslog(LOG_ERR, "vec_init fail\n");
-						cleanup(&cd);
-						return -1;
-					}
-					continue;
-				}
-			}
-
-			// clear buffer to prepare for next receive
-			vector_close(&send_vec);
 		}
-
-		vector_close(&recv_vec);
-		// Close client socket
-		close(new_fd);
 	}
 
 	if(sig_received){
